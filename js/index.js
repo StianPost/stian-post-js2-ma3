af74e0a8e135e92312fbeeff99f5a006d5c6df55
StianPost <69841774+StianPost@users.noreply.github.com>
2021-10-14 12:35:46 +0200

    Added the login

---
+++ js/index.js
@@ -0,0 +1,27 @@
+import {
+  getFromLocalStorage,
+  saveToLocalStorage,
+} from './libs/localStorageHelpers.js';
+import { testEmailAddress, testText } from './libs/validation.js';
+
+const login = document.querySelector('#loginForm');
+
+login.onsubmit = async () => {
+  event.preventDefault();
+  let email = document.querySelector('#email').value.trim();
+  let password = document.querySelector('#password').value.trim();
+
+  if (testEmailAddress(email) && testText(password, 5)) {
+    try {
+      console.log('passed');
+      const { data } = await axios.post('http://localhost:1337/auth/local', {
+        identifier: email,
+        password: password,
+      });
+      saveToLocalStorage('jwt', data.jwt);
+      saveToLocalStorage('user', data.user);
+
+      //   window.location.href = './profile.html';
+    } catch (error) {}
+  }
+};
---
+++ js/libs/validation.js
@@ -0,0 +1,14 @@
+export const testText = function (value, length) {
+  if (value.length < length) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+export const testEmailAddress = function (emailAddress) {
+  const regexString =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // if it passes will return true if it fails will return false
+  return regexString.test(emailAddress);
+};
